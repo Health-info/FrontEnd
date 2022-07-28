1f078bb9afcb2b033e7bf1eacc81c2205535db12
SUH <luckhome5@naver.com>
2022-07-28 17:41:08 +0900

    signUp Page 만들다 중단

--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -1,60 +1,55 @@
-import Style from "./signup.module.css";
-import React, { useState, useRef } from "react";
+import Style from "./signUp.module.css";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
   const [inputId, setInputId] = useState("");
   const [inputName, setInputName] = useState("");
   const [inputPw, setInputPw] = useState("");
+  const [inputPwCheck, setInputPwCheck] = useState("");
+  const [inputGender, setInputGender] = useState("");
+  const [inputAge, setInputAge] = useState("");
   const [inputHeight, setInputHeight] = useState("");
   const [inputWeight, setInputWeight] = useState("");
-  const [inputAge, setInputAge] = useState("");
-  //유효성 검사
-
-  //Focus용
-  const IdInput = useRef();
-  const NameInput = useRef();
-  const PwInput = useRef();
-  const HeightInput = useRef();
-  const WeightInput = useRef();
-  const AgeInput = useRef();
-
+  
+  //submit
   const submitHandler = () => {
-    if (inputId.length < 5) {
-      IdInput.current.focus();
-      return;
+    console.log("sign Up");
+  };
+
+  //password확인
+  const passwordCheckFunc = () => {
+    if(inputPw !== inputPwCheck){
+      if(!document.querySelector("#passwordCheckInput").className.includes(' is-invalid')){
+        document.querySelector("#passwordCheckInput").className += ' is-invalid';
+      }
     }
-    if (inputName.length < 2) {
-      NameInput.current.focus();
-      return;
-    }
-    if (inputPw.length < 5) {
-      PwInput.current.focus();
-      return;
-    }
-    if (inputHeight.length < 3) {
-      HeightInput.current.focus();
-      return;
-    }
-    if (inputWeight.length < 2) {
-      WeightInput.current.focus();
-      return;
-    }
-    if (inputAge.length < 1) {
-      AgeInput.current.focus();
+    else{
+      if(document.querySelector("#passwordCheckInput").className.includes(' is-invalid')){
+        document.querySelector("#passwordCheckInput").className = document.querySelector("#passwordCheckInput").className.replace(' is-invalid', '');
+      }
     }
   };
+  useEffect(passwordCheckFunc, [inputPwCheck]);
 
   //input data의 변화 있을 때마다 value값 변경하여 useState
   const handleInputId = (e) => {
     setInputId(e.target.value);
   };
-
+  const handleInputName = (e) => {
+    setInputName(e.target.value);
+  };
   const handleInputPw = (e) => {
     setInputPw(e.target.value);
   };
-  const handleInputName = (e) => {
-    setInputName(e.target.value);
+  const handleInputPwCheck = (e) => {
+    setInputPwCheck(e.target.value);
+  };
+  const handelInputGender = (e) => {
+    setInputGender(e.target.id);
+  }
+  const handleInputAge = (e) => {
+    setInputAge(e.target.value);
   };
   const handleInputHeight = (e) => {
     setInputHeight(e.target.value);
@@ -62,30 +57,15 @@ const SignUp = () => {
   const handleInputWeight = (e) => {
     setInputWeight(e.target.value);
   };
-  const handleInputAge = (e) => {
-    setInputAge(e.target.value);
-  };
-  // 체크박스 하나만 선택
-  const checkOnlyOne = (checkThis) => {
-    const checkboxes = document.getElementsByName("gender");
-    for (let i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i] !== checkThis) {
-        checkboxes[i].checked = false;
-      }
-    }
-  };
 
   return (
-    <div
-      className={Style.field}
-    >
+    <div className={Style.field}>
       <form>
         <h1>Sign Up</h1>
         {/* E-mail */}
         <div>
           <input
             className="basic"
-            ref={IdInput}
             type="email"
             name="input_id"
             value={inputId}
@@ -95,7 +75,6 @@ const SignUp = () => {
         </div>
         <input
           className="basic"
-          ref={NameInput}
           type="text"
           name="name"
           value={inputName}
@@ -106,7 +85,6 @@ const SignUp = () => {
         {/* Password */}
         <input
           className="basic"
-          ref={PwInput}
           type="text"
           name="input_pw"
           value={inputPw}
@@ -116,63 +94,64 @@ const SignUp = () => {
         <br />
         {/* Password Check */}
         <input
+          id="passwordCheckInput"
           className="basic"
           type="text"
           name="input_pw_check"
           placeholder="PassWord Check"
+          value={inputPwCheck}
+          onChange={handleInputPwCheck}
         />
         <br />
         {/* Gender Check */}
+        <label htmlFor="male">Male</label>
         <input
-          type="checkbox"
+          id="male"
+          type="radio"
           name="gender"
-          value="male"
-          onChange={(e) => checkOnlyOne(e.target)}
-          // Checkbox로 Focus주는 법 모르겠음
-        />{" "}
-        Male
-        <label htmlFor="cb1"></label>
-        <span></span>
+          onChange={handelInputGender}
+        />
+        <label htmlFor="female">Female</label>
         <input
-          type="checkbox"
+          id="female"
+          type="radio"
           name="gender"
-          value="female"
-          onChange={(e) => checkOnlyOne(e.target)}
-        />{" "}
-        <label htmlFor="cb2"></label>
-        Female
+          onChange={handelInputGender}
+        />
+        {/* Age Check */}
         <input
           className="age"
-          ref={AgeInput}
           onKeyPress={(event) => {
             if (!/[0-9]/.test(event.key)) {
               event.preventDefault();
             }
           }}
           placeholder="Age"
+          value={inputAge}
           onChange={handleInputAge}
         />
-        <br />
+        {/* Height Check */}
         <input
           className="height"
-          ref={HeightInput}
           onKeyPress={(event) => {
             if (!/[0-9]/.test(event.key)) {
               event.preventDefault();
             }
           }}
           placeholder="Height"
+          value={inputHeight}
           onChange={handleInputHeight}
         />
+        {/* Weight Check */}
         <input
           className="weight"
-          ref={WeightInput}
           onKeyPress={(event) => {
             if (!/[0-9]/.test(event.key)) {
               event.preventDefault();
             }
           }}
           placeholder="Weight"
+          value={inputWeight}
           onChange={handleInputWeight}
         />
         {/* Return & Submit  */}
