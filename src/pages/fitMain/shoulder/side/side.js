e4e9d20a555a3a608a8fa98d441a41768ff710b7
채지훈 <du@DU-2.local>
2022-09-06 12:26:50 +0900

    Page move without changing Url

--- src/pages/fitMain/shoulder/side/side.js
+++ src/pages/fitMain/shoulder/side/side.js
@@ -1,15 +1,10 @@
 import React from "react";
-import CableSideLateralRaise from "./detail/CableSideLateralRaise";
-import SideLateralRaise from "./detail/sideLateralRaise";
-import UprightRow from "./detail/upRightRow";
 function Side() {
   return (
     <div>
-      <button onClick={<SideLateralRaise />}>Side Lateral Raise</button>
-      <button onClick={<UprightRow />}>Uprigt Row</button>
-      <button onClick={<CableSideLateralRaise />}>
-        Cable Side Lateral Raise
-      </button>
+      <button>Side Lateral Raise</button>
+      <button>Uprigt Row</button>
+      <button>Cable Side Lateral Raise</button>
     </div>
   );
 }
